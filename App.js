369555adfb15d439b21535ad94f1d1418f3579b2
weswwn <wesley.qb.wen@gmail.com>
2019-08-12 15:42:54 -0700

    Added BoardMatrix

--- App.js
+++ App.js
@@ -32,6 +32,19 @@ col2Box2.addEventListener('click', (col2Box2) => placeMarker(col2Box2));
 //Event Listener for Column 1, Box 3
 var col2Box3= document.getElementById("col2_box3");
 col2Box3.addEventListener('click', (col2Box3) => placeMarker(col2Box3));
+//  ---------------------------------------------------------------------------
+
+//Event Listenter for Column 1, Box 1
+var col3Box1= document.getElementById("col3_box1");
+col3Box1.addEventListener('click', (col3Box1) => placeMarker(col3Box1));
+
+//Event Listener for Column 1, Box 2
+var col3Box2= document.getElementById("col3_box2");
+col3Box2.addEventListener('click', (col3Box2) => placeMarker(col3Box2));
+
+//Event Listener for Column 1, Box 3
+var col3Box3= document.getElementById("col3_box3");
+col3Box3.addEventListener('click', (col3Box3) => placeMarker(col3Box3));
 
 
 function placeMarker(clickedBox) {
@@ -67,9 +80,60 @@ function placeMarker(clickedBox) {
 
 function placeMarkerInMatrix(clickedDivID, XorO) {
     console.log(clickedDivID, XorO);
+    // ------- FIRST COLUMN -------------------
     if (clickedDivID === 'col1_box1') {
-        console.log('hi');
         boardMatrix[0][0].push(XorO);
+        console.log(boardMatrix);
+        checkForWinners(clickedDivID, XorO);
+    }
+    if (clickedDivID === 'col1_box2') {
+        boardMatrix[1][0].push(XorO);
+        console.log(boardMatrix);
+        checkForWinners(clickedDivID, XorO);
+    }
+    if (clickedDivID === 'col1_box3') {
+        boardMatrix[2][0].push(XorO);
+        console.log(boardMatrix);
+        checkForWinners(clickedDivID, XorO);
+    }
+    // ------- SECOND COLUMN -------------------
+    if (clickedDivID === 'col2_box1') {
+        boardMatrix[0][1].push(XorO);
+        console.log(boardMatrix);
+        checkForWinners(clickedDivID, XorO);
+    }
+    if (clickedDivID === 'col2_box2') {
+        boardMatrix[1][1].push(XorO);
+        console.log(boardMatrix);
+        checkForWinners(clickedDivID, XorO);
+    }
+    if (clickedDivID === 'col2_box3') {
+        boardMatrix[2][1].push(XorO);
+        console.log(boardMatrix);
+        checkForWinners(clickedDivID, XorO);
+    }
+    // ------- THIRD COLUMN -------------------
+    if (clickedDivID === 'col3_box1') {
+        boardMatrix[0][2].push(XorO);
+        console.log(boardMatrix);
+        checkForWinners(clickedDivID, XorO);
+    }
+    if (clickedDivID === 'col3_box2') {
+        boardMatrix[1][2].push(XorO);
+        console.log(boardMatrix);
+        checkForWinners(clickedDivID, XorO);
+    }
+    if (clickedDivID === 'col3_box3') {
+        boardMatrix[2][2].push(XorO);
+        console.log(boardMatrix);
+        checkForWinners(clickedDivID, XorO);
     }
 
+}
+
+checkForWinners = (clickedDivID, XorO) => {
+    // console.log('check winners');
+    // if ((boardMatrix[0][0][0] === true && boardMatrix[0][1][0] === true && boardMatrix[0][2][0] === true) || (boardMatrix[0][0][0] === false && boardMatrix[0][1][0] === false && boardMatrix[0][2][0] === false)) {
+    //     console.log('win');
+    
 }
