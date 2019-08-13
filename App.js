b5480d1c302a6ada84d0c49664cb5ca59294e4c8
weswwn <wesley.qb.wen@gmail.com>
2019-08-12 17:10:52 -0700

    Competed

--- App.js
+++ App.js
@@ -3,6 +3,7 @@
     //The value that keeps track of the current player will
     //reset to player1 which will then place an 'X' down
 var currentPlayer = true;
+var winner = '';
 var boardMatrix = [
              [[],[],[]],
              [[],[],[]],
@@ -55,15 +56,15 @@ function placeMarker(clickedBox) {
             //Place an X since it is the first Player
             document.getElementById(clickedBox.path[0].id)
             .textContent = 'X';
-            currentPlayer = !currentPlayer;
             placeMarkerInMatrix(clickedBox.path[0].id, true);
+            currentPlayer = !currentPlayer;
             
         } else {
             //Place an Y since it is the second Player
             document.getElementById(clickedBox.path[0].id)
             .textContent = 'O';
-            currentPlayer = !currentPlayer;
             placeMarkerInMatrix(clickedBox.path[0].id, false);
+            currentPlayer = !currentPlayer;
         }
     }
 }
@@ -72,12 +73,8 @@ function placeMarker(clickedBox) {
 
 
 
-// var boardMatrix = [
-//     [[],[],[]],
-//     [[],[],[]],
-//     [[],[],[]],
-//    ];
 
+var moveCount = 0;
 function placeMarkerInMatrix(clickedDivID, XorO) {
     console.log(clickedDivID, XorO);
     // ------- FIRST COLUMN -------------------
@@ -85,55 +82,236 @@ function placeMarkerInMatrix(clickedDivID, XorO) {
         boardMatrix[0][0].push(XorO);
         console.log(boardMatrix);
         checkForWinners(clickedDivID, XorO);
+        moveCount++;
     }
     if (clickedDivID === 'col1_box2') {
         boardMatrix[1][0].push(XorO);
         console.log(boardMatrix);
         checkForWinners(clickedDivID, XorO);
+        moveCount++;
     }
     if (clickedDivID === 'col1_box3') {
         boardMatrix[2][0].push(XorO);
         console.log(boardMatrix);
         checkForWinners(clickedDivID, XorO);
+        moveCount++;
     }
     // ------- SECOND COLUMN -------------------
     if (clickedDivID === 'col2_box1') {
         boardMatrix[0][1].push(XorO);
         console.log(boardMatrix);
         checkForWinners(clickedDivID, XorO);
+        moveCount++;
     }
     if (clickedDivID === 'col2_box2') {
         boardMatrix[1][1].push(XorO);
         console.log(boardMatrix);
         checkForWinners(clickedDivID, XorO);
+        moveCount++;
     }
     if (clickedDivID === 'col2_box3') {
         boardMatrix[2][1].push(XorO);
         console.log(boardMatrix);
         checkForWinners(clickedDivID, XorO);
+        moveCount++;
     }
     // ------- THIRD COLUMN -------------------
     if (clickedDivID === 'col3_box1') {
         boardMatrix[0][2].push(XorO);
         console.log(boardMatrix);
         checkForWinners(clickedDivID, XorO);
+        moveCount++;
     }
     if (clickedDivID === 'col3_box2') {
         boardMatrix[1][2].push(XorO);
         console.log(boardMatrix);
         checkForWinners(clickedDivID, XorO);
+        moveCount++;
     }
     if (clickedDivID === 'col3_box3') {
         boardMatrix[2][2].push(XorO);
         console.log(boardMatrix);
         checkForWinners(clickedDivID, XorO);
+        moveCount++;
     }
-
+    console.log(moveCount);
+    if (moveCount === 9) {
+        resetBoard();
+    }
 }
 
 checkForWinners = (clickedDivID, XorO) => {
     // console.log('check winners');
     // if ((boardMatrix[0][0][0] === true && boardMatrix[0][1][0] === true && boardMatrix[0][2][0] === true) || (boardMatrix[0][0][0] === false && boardMatrix[0][1][0] === false && boardMatrix[0][2][0] === false)) {
     //     console.log('win');
+// ---------------- ROW ONE WIN CONDITION --------------------
+    if (boardMatrix[0][0][0] === true && 
+        boardMatrix[0][1][0] === true &&
+        boardMatrix[0][2][0] === true ||
+        (boardMatrix[0][0][0] === false && 
+        boardMatrix[0][1][0] === false &&
+        boardMatrix[0][2][0] === false)) {
+            if (currentPlayer === true) {
+                winner = 'Player 1 Wins!!';
+            } else {
+                winner = 'Player 2 Wins!!';
+            }
+            setTimeout(() => {
+                alert(winner)
+                resetBoard();
+            } , 5)
+    }
+// ---------------- ROW TWO WIN CONDITION --------------------
+    if (boardMatrix[1][0][0] === true && 
+        boardMatrix[1][1][0] === true &&
+        boardMatrix[1][2][0] === true ||
+        (boardMatrix[1][0][0] === false && 
+        boardMatrix[1][1][0] === false &&
+        boardMatrix[1][2][0] === false)) {
+            if (currentPlayer === true) {
+                winner = 'Player 1 Wins!!';
+            } else {
+                winner = 'Player 2 Wins!!';
+            }
+            setTimeout(() => {
+                alert(winner)
+                resetBoard();
+            } , 5)
+    }
+
+// ---------------- ROW THREE WIN CONDITION --------------------
+    if (boardMatrix[2][0][0] === true && 
+        boardMatrix[2][1][0] === true &&
+        boardMatrix[2][2][0] === true ||
+        (boardMatrix[2][0][0] === false && 
+        boardMatrix[2][1][0] === false &&
+        boardMatrix[2][2][0] === false)) {
+            if (currentPlayer === true) {
+                winner = 'Player 1 Wins!!';
+            } else {
+                winner = 'Player 2 Wins!!';
+            }
+            setTimeout(() => {
+                alert(winner)
+                resetBoard();
+            } , 5)
+    }
+
+// ---------------- COLUMN ONE WIN CONDITION --------------------
+    if (boardMatrix[0][0][0] === true && 
+        boardMatrix[1][0][0] === true &&
+        boardMatrix[2][0][0] === true ||
+        (boardMatrix[0][0][0] === false && 
+        boardMatrix[1][0][0] === false &&
+        boardMatrix[2][0][0] === false)) {
+            if (currentPlayer === true) {
+                winner = 'Player 1 Wins!!';
+            } else {
+                winner = 'Player 2 Wins!!';
+            }
+            setTimeout(() => {
+                alert(winner)
+                resetBoard();
+            } , 5)
+    }
+    
+// ---------------- COLUMN TWO WIN CONDITION --------------------
+    if (boardMatrix[0][1][0] === true && 
+        boardMatrix[1][1][0] === true &&
+        boardMatrix[2][1][0] === true ||
+        (boardMatrix[0][1][0] === false && 
+        boardMatrix[1][1][0] === false &&
+        boardMatrix[2][1][0] === false)) {
+            if (currentPlayer === true) {
+                winner = 'Player 1 Wins!!';
+            } else {
+                winner = 'Player 2 Wins!!';
+            }
+            setTimeout(() => {
+                alert(winner)
+                resetBoard();
+            } , 5)
+    }
+// ---------------- COLUMN TWO WIN CONDITION --------------------
+    if (boardMatrix[0][2][0] === true && 
+        boardMatrix[1][2][0] === true &&
+        boardMatrix[2][2][0] === true ||
+        (boardMatrix[0][2][0] === false && 
+        boardMatrix[1][2][0] === false &&
+        boardMatrix[2][2][0] === false)) {
+            if (currentPlayer === true) {
+                winner = 'Player 1 Wins!!';
+            } else {
+                winner = 'Player 2 Wins!!';
+            }
+            setTimeout(() => {
+                alert(winner)
+                resetBoard();
+            } , 5)
+    }
+
+    // ---------------- MAJOR DIAGONAL WIN CONDITION --------------------
+    if (boardMatrix[0][0][0] === true && 
+        boardMatrix[1][1][0] === true &&
+        boardMatrix[2][2][0] === true ||
+        (boardMatrix[0][0][0] === false && 
+        boardMatrix[1][1][0] === false &&
+        boardMatrix[2][2][0] === false)) {
+            if (currentPlayer === true) {
+                winner = 'Player 1 Wins!!';
+            } else {
+                winner = 'Player 2 Wins!!';
+            }
+            setTimeout(() => {
+                alert(winner)
+                resetBoard();
+            } , 5)
+    }
+    // ---------------- MINOR DIAGONAL WIN CONDITION --------------------
+    if (boardMatrix[0][2][0] === true && 
+        boardMatrix[1][1][0] === true &&
+        boardMatrix[2][0][0] === true ||
+        (boardMatrix[0][2][0] === false && 
+        boardMatrix[1][1][0] === false &&
+        boardMatrix[2][0][0] === false)) {
+            if (currentPlayer === true) {
+                winner = 'Player 1 Wins!!';
+            } else {
+                winner = 'Player 2 Wins!!';
+            }
+            setTimeout(() => {
+                alert(winner)
+                console.log('hi');
+                resetBoard();
+            } , 5)
+    
+    }
+    
+}
+var resetBoard = () => {
+    currentPlayer = true;
+    winner = '';
+    moveCount = 0;
+    boardMatrix = [
+            [[],[],[]],
+            [[],[],[]],
+            [[],[],[]],
+        ];
+    console.log(boardMatrix);
+    // console.log(document.getElementById('col1').children);
+    var col1 = document.getElementById('col1').children
+    var col2 = document.getElementById('col2').children
+    var col3 = document.getElementById('col3').children
+    
+    for (var i = 0; i < col1.length; i++) {
+        document.getElementById(col1[i].id).innerHTML = "";
+    }
+
+    for (var i = 0; i < col2.length; i++) {
+        document.getElementById(col2[i].id).innerHTML = "";
+    }
+    for (var i = 0; i < col3.length; i++) {
+        document.getElementById(col3[i].id).innerHTML = "";
+    }
     
 }
